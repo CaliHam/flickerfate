4ebe34630c2efd88e19ef6cbffab8be6d7acba01
Calli Herrmann <calliherrmann@gmail.com>
2023-07-09 15:28:58 -0400

    Add functionality to load response

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
 import './App.css';
 import { useState } from 'react';
 import Form from '../Form/Form'
+import Response from '../Response/Response';
 
 const App = () => {
 
-  // const [] = useState('')
+  const [submitted, setSubmitted] = useState(null)
 
   return (
     <div className="App">
       <h1>FlickerFate</h1>
       <h2><b>Rules:</b> Ask a yes-or-no question and fate will decide your response!</h2>
-      <Form />
+      <Form submitted={submitted} setSubmitted={setSubmitted}/>
+      {submitted && <Response question={submitted}/>}
     </div>
   );
 }
--- src/components/Response/Response.jsx
+++ src/components/Response/Response.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import './Response.css'
+import { getAnswer } from '../../apicalls/apicalls'
 
-const Response = () => {
 
+const Response = ({ question }) => {
+
+    const [answer, setAnswer] = useState(null)
+
+    useEffect(() => {
+        getAnswer().then(data => setAnswer(data))
+        .catch(err => console.log(err))
+    }, [question])
+
+    const renderResponse = (answer) => {
+        return (
+            <div className='response-container'>
+                <p>{answer.answer}</p>
+                <img src={answer.image}></img>
+            </div>
+        )
+    }
 
     return (
-        <>
-        </>
+        <>{!answer ? 'Loading' : renderResponse(answer)}</>
     )
 }
 
