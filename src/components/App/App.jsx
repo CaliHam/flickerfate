158c8967e9f0d80d16d8583546b0ae4ad3ea9b8f
Calli Herrmann <calliherrmann@gmail.com>
2023-07-09 14:13:59 -0400

    Add basic app and form layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
+import { useState } from 'react';
+import Form from '../Form/Form'
 
-function App() {
+const App = () => {
+
+
   return (
     <div className="App">
+      <h1>FlickerFate</h1>
+      <h2><b>Rules:</b> Ask a yes-or-no question and fate will decide your response!</h2>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import './Form.css';
+
+const Form = () => {
+    
+
+    return (
+        <form>
+            <input placeholder='Ask your question here'></input>
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
+
+export default Form;
